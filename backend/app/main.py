b831c79b15d5a8492b5d2969c45b7a79b8cff3bf
Hungdinhz <phunghung12112005@gmail.com>
2026-04-15 22:53:42 +0700

    fix: allow  localhost access to backend render

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.models import user, post, interaction, friend # Import để SQLAlchemy nhận diện được Model User
 from app.api import user as user_api, post as post_api, friend as friend_api # Import router của User, Post và Friend
+from fastapi.middleware.cors import CORSMiddleware
 
 # Lệnh này sẽ yêu cầu SQLAlchemy kiểm tra DB, nếu chưa có bảng thì tự động tạo!
 Base.metadata.create_all(bind=engine)
@@ -11,6 +12,21 @@ app = FastAPI(
     description="API cho mạng xã hội thu nhỏ",
     version="1.0.0"
 )
+# --- THÊM ĐOẠN NÀY ĐỂ SỬA LỖI CORS ---
+# Danh sách các tên miền được phép gọi API
+origins = [
+    "http://localhost:3000",      # Cho phép Frontend chạy ở local gọi lên
+    # "https://domain-frontend-cua-ban.com", # Sau này deploy Frontend thì thêm link vào đây
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # Cấp phép cho các domain trong danh sách
+    allow_credentials=True,
+    allow_methods=["*"],          # Cho phép mọi phương thức (GET, POST, PUT, DELETE...)
+    allow_headers=["*"],          # Cho phép mọi loại Header (đặc biệt là cái Authorization chứa Token)
+)
+# -------------------------------------
 
 # Nhúng router của User vào ứng dụng
 app.include_router(user_api.router)
