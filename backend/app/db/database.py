89fb232777859e866da278d0b24d575d104f3746
Hungdinhz <phunghung12112005@gmail.com>
2026-04-15 23:34:14 +0700

    fix: loi dang ki

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,8 +3,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
 # 1. Tạo "Động cơ" (Engine) kết nối tới SQL Server bằng URL lấy từ file .env
-engine = create_engine(settings.DATABASE_URL)
-
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,  # BẮT BUỘC: "Thử mic" (ping) xem DB còn sống không trước khi gửi lệnh
+    pool_recycle=300     # TÙY CHỌN: Tự động dọn dẹp các kết nối cũ sau mỗi 5 phút (300 giây)
+)
 # 2. Tạo một nhà máy sản xuất các "Phiên làm việc" (Session) với DB 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
